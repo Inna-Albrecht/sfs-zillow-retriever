8f90557bc22a6b88155f2f4880bc28636685bc58
Inna-Albrecht <thisisnotkatiswear@gmail.com>
2024-09-26 10:48:31 -0500

    Initial commit

---
+++ puppet.js
@@ -0,0 +1,42 @@
+import chromium from 'chrome-aws-lambda'
+
+export const handler = async (event, context) => {
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    })
+
+    const page = await browser.newPage()
+
+    const { url } = event.queryStringParameters;
+
+    try {
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        await page.setViewport({ width: 1080, height: 1024 });
+
+
+        const zestimate = await page.$eval(
+            'span.Text-c11n-8-99-3__sc-aiai24-0.dFhjAe span',
+            el => el.innerText
+        );
+
+        return {
+            statuscode: 200,
+            body: JSON.stringify({ message: zestimate })
+        }
+    } catch (error) {
+        console.error('Error:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to extract info' }),
+        };
+    } finally {
+        await browser.close()
+    }
+
+
+
+}
