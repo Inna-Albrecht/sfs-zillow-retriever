ce4a83a033ca0974370d4c37181f55301ce05f9b
Inna-Albrecht <thisisnotkatiswear@gmail.com>
2024-10-18 08:26:38 -0500

    add 3 fields to scraping package

--- puppet.js
+++ puppet.js
@@ -25,14 +25,16 @@ export const handler = async (event, context) => {
     executablePath: isLambda
       ? await chromium.executablePath
       : puppeteer.executablePath(),
-    headless: true,
+    headless: false,
   });
 
   const page = await browser.newPage();
 
   try {
-    await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.setViewport({ width: 1080, height: 1024 });
+    await page.goto(url);
+
+    // Wait for the `ul.dpf__sc-xzpkxd-0.fZHAmk` element to be available
+    await page.waitForSelector('ul.dpf__sc-xzpkxd-0.fZHAmk');
 
     const zestimate = await page.evaluate(() => {
       const spans = Array.from(document.querySelectorAll('span'));
@@ -41,25 +43,32 @@ export const handler = async (event, context) => {
     });
 
     const data = await page.evaluate(() => {
-      let spans;
+      const targetSpans = Array.from(document.querySelectorAll('span.Text-c11n-8-99-3__sc-aiai24-0.dFxMdJ'));
+
+      const bed = targetSpans[0] ? targetSpans[0].innerText : 'Beds not found';
+      const baths = targetSpans[1] ? targetSpans[1].innerText : 'Baths not found';
+      const squareFootage = targetSpans[2] ? targetSpans[2].innerText : 'Square footage not found';
+
       const ulElement = document.querySelector('ul.dpf__sc-xzpkxd-0.fZHAmk');
-      const targetSpans = Array.from(ulElement.querySelectorAll('span')).filter(span =>
+      const propertySpans = Array.from(ulElement.querySelectorAll('span')).filter(span =>
         span.className.includes('kOlNqB')
       );
 
-      const spanTexts = targetSpans.map(span => span.innerText);
+      const spanTexts = propertySpans.map(span => span.innerText);
 
       const propertyType = spanTexts[0] || 'Property type not found';
       const yearBuilt = spanTexts[1] || 'Year built not found';
       const acreage = spanTexts[spanTexts.length - 1] || 'Acreage not found';
+
       return {
         propertyType,
         yearBuilt,
-        acreage
+        acreage,
+        bed,
+        baths,
+        squareFootage,
       };
     });
-
-    console.log('%cpuppet.js:49 data', 'color: #007acc;', data);
 
     return {
       statusCode: 200,
@@ -70,7 +79,6 @@ export const handler = async (event, context) => {
       }),
     };
   } catch (error) {
-    console.error('Error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({
